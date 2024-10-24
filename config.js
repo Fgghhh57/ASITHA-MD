9ffbcb12e0e9e2d16f408c9792f9c0086db137cd
Fgghhh57 <gfhgh0149@gmail.com>
2024-10-24 23:10:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XzQBxKpC#7EZOT_81ZruvuiiWxsgHn9QphxnZI1H0z1BMgVtX5lE",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7ngilBwQ#OTPYsmSwFoULQ4ISFSRdrfjsxh_hlPymr3EFNp2k5W8",
 MONGODB: process.env.MONGODB || "https://downloads.mongodb.com/compass/mongodb-compass-1.44.5-win32-x64.exe",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
